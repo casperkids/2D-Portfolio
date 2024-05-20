71d108986b9917d18d5c10f4384641414d2b7987
casperkids <yukishiroi@protonmail.com>
2024-05-20 16:28:19 +0200

    changed some my github

--- src/constants.js
+++ src/constants.js
@@ -3,14 +3,14 @@ export const scaleFactor = 4
 export const dialogueData = {
   pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
     I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+    Here is my <a href="https://github.com/casperkids" target="_blank">Github</a>!`,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
   "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
+  I also make game programming tutorials on YouTube. Go sub to <a href="https://github.com/casperkids" target="_blank">my channel</a>! (If you like the content)
   You'll learn how I built this portfolio you're currently playing through!`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+   <a href="https://github.com/casperkids" target="_blank">Theprimeagen</a>, <a href="https://github.com/casperkids" target="_blank">Theo - t3.gg</a>,
+  <a href="https://github.com/casperkids" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://github.com/casperkids" target="_blank">Melkey</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
   Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
