7e5037c7821c32efefc3232237e7c255f4b195da
casperkids <yukishiroi@protonmail.com>
2024-05-26 10:39:14 +0200

    update links

--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,13 @@
 export const scaleFactor = 4
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/casperkids" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://github.com/casperkids" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+  pc: `This is my PC. Lately, I've been studying UI/UX design, coding, and occasionally dabbling in illustration. While I enjoy working on projects digitally, I also have a fondness for creating analog art.! mygithub page <a href="https://github.com/casperkids" target="_blank">`,
+  "cs-degree": `I've just started my studies, but I'm eager to expand my knowledge further in the future. My linkedin <a href="https://www.linkedin.com/in/yuki-shiroi-5609124/" target="_blank">`,
+  "sofa-table": `That's my sofa. I take power naps, observe cats, and spend time lost in imagination.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://github.com/casperkids" target="_blank">Theprimeagen</a>, <a href="https://github.com/casperkids" target="_blank">Theo - t3.gg</a>,
   <a href="https://github.com/casperkids" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://github.com/casperkids" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  bed: `Here is my Bed, I reflect on the day's events, express gratitude, and drift off to sleep. I usually avoid screen time, but occasionally, I indulge in playing on my Switch.`,
   resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
   Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
@@ -19,5 +15,5 @@ export const dialogueData = {
   Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
   library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit Yuki's portfolio, just close the tab.`,
 };
