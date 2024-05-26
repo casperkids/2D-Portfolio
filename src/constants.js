ff9268e34d46c8922a75bcfb7cc9366814215d6c
casperkids <yukishiroi@protonmail.com>
2024-05-26 19:44:28 +0200

    updated links and touch function main.js

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,14 @@
 export const scaleFactor = 4
 
 export const dialogueData = {
-  pc: `This is my PC. Lately, I've been studying UI/UX design, coding, and occasionally dabbling in illustration. While I enjoy working on projects digitally, I also have a fondness for creating analog art.! mygithub page <a href="https://github.com/casperkids" target="_blank">`,
-  "cs-degree": `I've just started my studies, but I'm eager to expand my knowledge further in the future. My linkedin <a href="https://www.linkedin.com/in/yuki-shiroi-5609124/" target="_blank">`,
-  "sofa-table": `That's my sofa. I take power naps, observe cats, and spend time lost in imagination.`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://github.com/casperkids" target="_blank">Theprimeagen</a>, <a href="https://github.com/casperkids" target="_blank">Theo - t3.gg</a>,
-  <a href="https://github.com/casperkids" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://github.com/casperkids" target="_blank">Melkey</a>!`,
+  pc: `This is my PC. Lately, I've been studying UI/UX design, coding, and occasionally dabbling in illustration. While I enjoy working on projects digitally, I also have a fondness for creating analog art! my github page <a href="https://github.com/casperkids" target="_blank">github (casperkids)</a>.`,
+  "cs-degree": `I've just started my studies, but I'm eager to expand my knowledge further in the future. My linkedin <a href="https://www.linkedin.com/in/yuki-shiroi-5609124/details/certifications/" target="_blank">certifications</a>.`,
+  "sofa-table": `That's my sofa. I take power naps, observe cats🐱, and spend time lost in imagination.`,
+  tv: `That's my imaginary TV. Sometimes, information that randomly appears on TV can also serve as inspiration. `,
   bed: `Here is my Bed, I reflect on the day's events, express gratitude, and drift off to sleep. I usually avoid screen time, but occasionally, I indulge in playing on my Switch.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit Yuki's portfolio, just close the tab.`,
+  resume: `This is my main workspace. You can view my portfolio here! <a href="https://yukinando.com/" target="_blank">Check it out?</a> Feel free to contact me at yukishiroi@protonmail.com`,
+  projects: `I created a memo app that allows you to jot down daily notes.
+The app not only lets you leave memos but also generates a robot based on the title of the day, allowing you to experience both the memo function and a sense of play. Additionally, it includes a footprint feature similar to GitHub, enabling you to track your daily progress. <a href="https://casperkids.github.io/LogApp/">Log App here</a>.`,
+  library: `This is my bookshelf. I read a variety of books 📚 including those on natural pattern formation, ethnology, art collections, and manga. Recently, a friend gave me "About Face," and I have been reading it, but it feels like I'll never finish it.`,
+  exit: `If you want to exit Yuki's portfolio, just close the tab. Thanks for visiting! x`,
 };
--- src/main.js
+++ src/main.js
@@ -91,6 +91,7 @@ k.onResize(() => {
 k.onUpdate(() => {
     k.camPos(player.pos.x, player.pos.y +100 );
 })
+
 k.onMouseDown((mouseBtn) =>{
     if (mouseBtn !== "left" || player.isInDialogue) return;
 
@@ -136,18 +137,71 @@ k.onMouseDown((mouseBtn) =>{
         return
     }
 })
-    k.onMouseRelease(() => {
-        if (player.direction === "down"){
-            player.play("idle-down");
-            return
-        }
-        if (player.direction ==="up") {
-            player.play("idle-up");
-            return
-        }
-        player.play("idle-side")
-    })
-        
-})
+    function stopAnims() {
+    if (player.direction === "down") {
+      player.play("idle-down");
+      return;
+    }
+    if (player.direction === "up") {
+      player.play("idle-up");
+      return;
+    }
 
-k.go("main")
+    player.play("idle-side");
+  }
+
+  k.onMouseRelease(stopAnims);
+
+  k.onKeyRelease(() => {
+    stopAnims();
+  });
+  k.onKeyDown((key) => {
+    const keyMap = [
+      k.isKeyDown("right"),
+      k.isKeyDown("left"),
+      k.isKeyDown("up"),
+      k.isKeyDown("down"),
+    ];
+
+    let nbOfKeyPressed = 0;
+    for (const key of keyMap) {
+      if (key) {
+        nbOfKeyPressed++;
+      }
+    }
+
+    if (nbOfKeyPressed > 1) return;
+
+    if (player.isInDialogue) return;
+    if (keyMap[0]) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "right";
+      player.move(player.speed, 0);
+      return;
+    }
+
+    if (keyMap[1]) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "left";
+      player.move(-player.speed, 0);
+      return;
+    }
+
+    if (keyMap[2]) {
+      if (player.curAnim() !== "walk-up") player.play("walk-up");
+      player.direction = "up";
+      player.move(0, -player.speed);
+      return;
+    }
+
+    if (keyMap[3]) {
+      if (player.curAnim() !== "walk-down") player.play("walk-down");
+      player.direction = "down";
+      player.move(0, player.speed);
+    }
+  });
+});
+
+k.go("main");
